31a4c344c35da49d35cdcfb2c547f7832fb2f204
Kalugin Nikolay <nkalugin@marathonbet.ru>
2016-06-07 13:45:54 +0300

    First Week Impl: 10/10

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,56 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    * n == r
+    * k == c
+    *
+    * (n, k) = (n-1, k-1) + (n-1, k)
+    *
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (r < 0 || c < 0)
+      0
+    else if (c < 1 || r < 1 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    *
+    **/
+  def balance(chars: List[Char]): Boolean = {
+    def balance0(counterLeft: Int, counterRight: Int, chars: List[Char]): Int = {
+      if (counterRight > counterLeft) 1
+      else chars match {
+        case c :: cs =>
+          if (c == '(') balance0(counterLeft + 1, counterRight, cs)
+          else if (c == ')') balance0(counterLeft, counterRight + 1, cs)
+          else balance0(counterLeft, counterRight, cs)
+        case _ => counterLeft - counterRight
+      }
+    }
+    balance0(0, 0, chars) == 0
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChange0(money: Int, reversedCoins: List[Int]): Int = {
+      reversedCoins match {
+        case largest :: tail =>
+          if (money - largest == 0)
+            1 + countChange(money, tail)
+          else if (money - largest > 0)
+            countChange(money - largest, reversedCoins) + countChange(money, tail)
+          else
+            countChange(money, tail)
+        case _ => 0
+      }
+    }
+    countChange0(money, coins.sorted.reverse)
+  }
+}
